b976052132cde2e31efe632b72882a739fa2878a
jimnyy <dojimny1004@gmail.com>
2024-07-19 03:27:05 +0900

    [Refactor] 업체 조회 기능 성능 개선

--- project/src/main/java/org/example/fourtreesproject/company/repository/CompanyRepository.java
+++ project/src/main/java/org/example/fourtreesproject/company/repository/CompanyRepository.java
@@ -2,9 +2,11 @@ package org.example.fourtreesproject.company.repository;
 
 import org.example.fourtreesproject.company.model.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    @Query("SELECT c FROM Company c JOIN FETCH c.user u WHERE u.idx = :userIdx")
     Optional<Company> findByUserIdx(Long userIdx);
 }
